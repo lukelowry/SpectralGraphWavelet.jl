4cd6de2462b766094ba7b87a5e79ab5c0241800b
Wyatt Lowery <wyattluke.lowery@gmail.com>
2025-12-30 15:06:15 -0600

    Package Content

--- src/SpectralGraphWavelet.jl
+++ src/SpectralGraphWavelet.jl
@@ -1,5 +1,15 @@
 module SpectralGraphWavelet
 
-# Write your package code here.
+using MAT
+using SparseArrays
+using LinearAlgebra
+using SuiteSparse
+using Statistics
+
+export load_laplacian, load_signal
+export VFKern, DyConvolve, impulse, convolve, lowpass, bandpass, highpass, addbranch!
+
+include("io.jl")
+include("dynamic.jl")
 
 end
